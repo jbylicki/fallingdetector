630e252850f206bc20b5490672c3759ffae35362
Patryk Koscik <koscikpatryk@gmail.com>
2023-05-31 13:57:02 +0200

    [cdc] Fix UART over CDC
    At least I think so

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include "main_functions.hpp"
 #include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
+#include <zephyr/drivers/uart.h>
+
+USBD_DEVICE_DEFINE(sample_usbd,
+		   DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)),
+		   0x2fe3, 0x0001);
+
+
 int main(int argc, char *argv[])
 {
+		if (usb_enable(NULL)) {
+		return 1;
+	}
+
   printk("Starting TensorFlow Lite Micro\n");
 	setup();
 	while (true) {
